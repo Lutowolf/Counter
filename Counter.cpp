5e3533f9c7117068343b858051776f2170cb15fd
LutowolfPC <Lazycatworld@gmail.com>
2023-09-22 14:37:57 +0500

    isprevlenia

--- Counter.cpp
+++ Counter.cpp
@@ -5,18 +5,12 @@ using namespace std;
 
 class Counter {
 private:
-    int Starting_Number;
+    int Starting_Number = 1;
 public:
-    Counter() {
-        this->Starting_Number = 1;
-    }
     Counter(int Input) {
         this->Starting_Number = Input;
     }
-    void Set_Starting_Number(int Input)
-    {
-            this->Starting_Number = Input;
-    }
+    Counter() {}
     void increase() { this->Starting_Number++; }
     void decrease() { this->Starting_Number--; }
     void show() { cout << this->Starting_Number << endl; }
@@ -27,13 +21,13 @@ int main()
     setlocale(LC_ALL, "Russian");
     Counter counter;
     string FirstAnswer;
-    int Input = 0;
     cout << "Вы хотите указать начальное значение счётчика? Введите \"yes\" или \"no\": ";
     cin >> FirstAnswer;
     if (FirstAnswer == "yes") {
         cout << "введите начальное значение счетчика: ";
+        int Input;
         cin >> Input;
-        counter.Set_Starting_Number(Input);
+        counter = Counter(Input);
     }
     string UserCommand= " ";
     while (UserCommand != "close")
